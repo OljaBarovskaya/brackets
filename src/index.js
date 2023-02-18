2c93e1ebaa2c232b05252face91844b1273dddd6
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-02-18 23:17:50 +0300

    brackets partly done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let i=0; i<str.length; i++){
+    let currentElement = str[i];
+    for (let j=0; j<bracketsConfig.length; j++){
+      if (currentElement === bracketsConfig[j][0]){
+        arr.push(str[i]);
+      } else {
+        if (arr.length === 0){return false}
+        let topElement = arr[arr.length-1];
+        if (topElement === bracketsConfig[j][0]) {
+          arr.pop()
+        } else {return false}
+      }
+    }
+    
+  }
+  return arr.length === 0;
 }
